f148801dc991a3cdc7b2231e7b0e7d45ac4c5ac9
Jose Añasco <joseanasco1@gmail.com>
2023-07-23 15:01:55 +0200

    by word flaschards generation

--- anki_nrk.rb
+++ anki_nrk.rb
@@ -4,6 +4,7 @@ gemfile do
   source 'https://rubygems.org'
 
   gem 'anki2'
+  gem 'anki'
   gem 'faraday'
   gem 'webvtt-ruby'
   gem 'dotenv'
@@ -11,6 +12,7 @@ end
 
 require 'dotenv'; Dotenv.load
 require 'anki2'
+require 'anki'
 require "faraday"
 require "json"
 require "webvtt"
@@ -19,10 +21,8 @@ def create_anki_cards_by_subs(show:, season:, episode: )
   @anki_subtitles = Anki2.new(name: "#{show}: Season #{season} Episode #{episode}", output_path: "./#{show}_#{season}_#{episode}.apkg")
 
   prf_id = get_prf_id(show, season, episode)
-  prefix = prf_id[0...6]
-  cache = prf_id[6...8]
 
-  nrk_subs(prf_id, prefix, cache).each do |sub|
+  nrk_subs(prf_id).each do |sub|
     translation = translate(sub.text)
 
     p "start: #{sub.start}"
@@ -36,6 +36,32 @@ def create_anki_cards_by_subs(show:, season:, episode: )
   @anki_subtitles.save
 end
 
+def create_anki_cards_by_words(show:, season:, episode: )
+  prf_id = get_prf_id(show, season, episode)
+
+  # cleanup: get rid of easy words, duplicates, etc
+  words = nrk_subs(prf_id)
+    .map(&:text)
+    .map { _1.gsub(/[^a-zA-Z]/, " ") }
+    .map(&:split)
+    .flatten
+    .map(&:downcase)
+    .map(&:strip)
+    .uniq
+    .select { _1.length > 3 }
+
+  card_headers = [ "front", "back" ]
+
+  p "translating words..."
+  card_data = words.map do |word|
+    { "front" => word, "back" => translate(word) }
+  end
+
+  p "generating anki card"
+  Anki::Deck.new(card_headers:, card_data: ).generate_deck(file: './norwegian_words.txt')
+  p "done"
+end
+
 def get_prf_id(show, season, episode)
   Faraday.get("https://psapi.nrk.no/tv/catalog/series/#{show}")
   .body
@@ -43,7 +69,10 @@ def get_prf_id(show, season, episode)
   .then { _1.dig('_embedded', 'seasons', season, '_embedded', 'episodes', episode, 'prfId') }
 end
 
-def nrk_subs(prf_id, prefix ,cache )
+def nrk_subs(prf_id)
+  prefix = prf_id[0...6]
+  cache = prf_id[6...8]
+
   link = "https://undertekst.nrk.no/prod/%{prefix}/%{dir}/%{prfid}/%{lang}/%{prfid}.vtt"
   real_link = link % {prfid: prf_id, prefix: prefix, dir: cache, lang: "TTV"}
   subs = Faraday.get(real_link).body
@@ -61,4 +90,9 @@ def translate(text)
   .first['translatedText']
 end
 
-create_anki_cards_by_subs(show: ARGV[0], season: ARGV[1].to_i, episode: ARGV[2].to_i)
+if ARGV[3] == 'words'
+  create_anki_cards_by_words(show: ARGV[0], season: ARGV[1].to_i, episode: ARGV[2].to_i)
+else
+  create_anki_cards_by_subs(show: ARGV[0], season: ARGV[1].to_i, episode: ARGV[2].to_i)
+end
+
